0b43ff186482d692d179017a9960fdd838f7f516
desyhzsh71 <desiekaa71@student.ub.ac.id>
2025-11-03 15:51:39 +0700

    Delete src/controllers directory

--- src/controllers/project/index.ts
+++
@@ -1,6 +0,0 @@
-export { createProject } from './create';
-export { getProjects, getProjectById } from './get';
-export { updateProject } from './update';
-export { deleteProject } from './delete';
-export { duplicateProject, duplicateProjectToOrganization } from './duplicate';
-export { updateCustomDomain } from './customDomain';
