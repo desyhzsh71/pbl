9f880b1f5641345c13aacf2494403a4ea71d1e0d
desyhzsh71 <desiekaa71@student.ub.ac.id>
2025-11-03 15:36:04 +0700

    Delete src/controllers directory

--- src/controllers/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { register } from './register';
-export { login } from './login';
-export { profile } from './profile';
-export { forgotPassword, verifyResetToken, resetPassword } from './password';
--- src/controllers/plan/index.ts
+++
@@ -1,5 +0,0 @@
-export { createPlan } from "./create";
-export { getAllPlans } from "./getAll";
-export { getPlanById } from "./getById";
-export { updatePlan } from "./update";
-export { deletePlan } from "./delete";
